7375c0394ee8d1ad91a8fed1ffd465ae0362b6a8
Joao Conceicao <joaogabrieloliveira81@gmail.com>
2022-02-01 12:10:28 -0300

    Improve and Fix Pokedex Section

--- src/pages/Home/components/letra.jsx
+++ src/pages/Home/components/letra.jsx
@@ -1,22 +1,18 @@
 import React, { Component } from 'react'
-import { Alert } from 'react-alert';
-
 
 export default props =>
-<div className="poke">
+<section id="pokedex">
+    <img src="https://thumbs.gfycat.com/WeakSparseDromedary-small.gif"/>
+
     <div>
-    <img className="img2" src="https://thumbs.gfycat.com/WeakSparseDromedary-small.gif"/>
-    <div className="tekst" >
-    
-<strong className="titu" >O que é Pokédex?</strong>
-    <div>
-    A Pokédex, também conhecida como Poké-Agenda aqui no Brasil é uma enciclopédia virtual portátil de alta tecnologia  que os treinadores<br></br>
-   Pokémon transportam para registra todas as espécies diferentes de Pokémon que são encontradas durante a sua viagem como treinadores. <br></br>
-   Em geral, quando são cumpridos determinados requisitos, a capacidade pode ser aumentada permitindo que a Pokédex possa armazenar dados de outros Pokémon. <br></br>
-  </div> 
-  </div> 
-   <div className="btn1">
-    <a href="/Pokemons" className="btn"  ><strong>ver pokedex </strong></a >
-        </div>
-     </div>
-</div>
+        <h2> <strong>O que é Pokédex?</strong></h2>
+
+        <p>A Pokédex, também conhecida como Poké-Agenda aqui no Brasil é uma enciclopédia virtual portátil de alta tecnologia  que os treinadores</p>
+
+        <p>Pokémon transportam para registra todas as espécies diferentes de Pokémon que são encontradas durante a sua viagem como treinadores.</p>
+
+        <p>Em geral, quando são cumpridos determinados requisitos, a capacidade pode ser aumentada permitindo que a Pokédex possa armazenar dados de outros Pokémon.</p>
+
+        <a href="/Pokemons" className="btn"><strong>Ver Pokedex!</strong></a >
+    </div>
+</section>
